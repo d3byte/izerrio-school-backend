67098c17adb4dcee100e0136663c360fa58c6c73
Sergey Savtyra <pandinator@protonmail.com>
2018-07-18 01:51:11 +0300

    Модель заявки

---
+++ src/models/Application.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const applicationSchema = new Schema({
+    sum: { type: Number, required: true },
+    system: { type: String, required: true },
+    account: { type: String, required: true },
+    teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true},
+    isDone : { type: Boolean, default: false },
+})
+
+const Application = mongoose.model('Application', applicationSchema)
+
+export default Application
