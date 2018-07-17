5e0d93fae02231c3a35d51a65963027c2cd02406
Sergey Savtyra <pandinator@protonmail.com>
2018-07-18 01:49:34 +0300

    Модель рядового пользователя с вариацией на ученика и админа

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,11 @@ const userSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     avatar: { type: String, required: true },
-    courses: [{ type: Schema.Types.ObjectId, ref: 'Course', default: null }]
+    subjects: [{
+        teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true },
+        subject: { type: Schema.Types.ObjectId, ref: 'Subject', required: true },
+    }],
+    isAdmin: { type: Boolean, default: true },
 })
 
 const User = mongoose.model('User', userSchema)
