e90c6bae5577fd1c4e133586678509d0ca4bace8
Sergey Savtyra <pandinator@protonmail.com>
2018-07-19 23:22:00 +0300

    Адаптация юзера под подписочную модель, возможность стать наставником

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,11 @@
 import mongoose from 'mongoose'
+import moment from 'moment'
+
+const addRealMonth = (d: any) => {
+    var fm = moment(d).add(1, 'M');
+    var fmEnd = moment(fm).endOf('month');
+    return d.date() != fm.date() && fm.isSame(fmEnd.format('YYYY-MM-DD')) ? fm.add(1, 'd') : fm;  
+}
 
 const { Schema } = mongoose
 
@@ -10,8 +17,12 @@ const userSchema = new Schema({
     subjects: [{
         teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true },
         subject: { type: Schema.Types.ObjectId, ref: 'Subject', required: true },
+        validUntil: { type: Date, default: addRealMonth(moment()) }
     }],
-    isAdmin: { type: Boolean, default: true },
+    teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', default: null },
+    students: [{ type: Schema.Types.ObjectId, ref: 'User', default: null }],
+    isAdmin: { type: Boolean, default: false },
+    isHelper: { type: Boolean, default: false },
 })
 
 const User = mongoose.model('User', userSchema)
