59300910c9c05e8695ae510c477164c353751986
Sergey Savtyra <pandinator@protonmail.com>
2018-07-20 00:20:48 +0300

    Пофиксил крон

--- cron/index.ts
+++ cron/index.ts
@@ -1,25 +1,30 @@
 import cron from 'cron'
 import moment from 'moment'
 import db from '../src/models'
+import { resolve } from 'path';
+import { resolveCname } from 'dns';
 const { User } = db
 
 export const removeInvalidUserSubscriptions = new cron.CronJob({
     cronTime: '00 00 00 * * *',
     async onTick() {
         const users: any = await User.find().populate('subjects')
+        let promises: Array<Promise<any>> = []
         users.map((user: any) => {
             let subjectsToRemove: Array<string> = []
-            for (let subject of user.subjects) {
-                const difference = moment().diff(moment(subject.validUntil))
-                console.log(difference)
-                if (difference < 0) {
-                    subjectsToRemove.push(subject.id)
+            for (let item of user.subjects) {
+                const difference = moment().diff(item.validUntil)
+                if (difference > 0) {
+                    subjectsToRemove.push(item.subject)
                 }
             }
-            user.subjects = user.subjects.filter((subject: any) => !subjectsToRemove.includes(subject._id))
-            user.save()
+            user.subjects = user.subjects.filter((item: any) => !subjectsToRemove.includes(item.subject))
+            promises.push(user.save())
+        })
+        Promise.all(promises).then(async res => {
+            console.log('Пользователи после обработки')
+            console.log(await User.find().populate('subjects'))
         })
     },
-    // Удалить после теста
-    runOnInit: true
+    runOnInit: process.env.NODE_ENV === 'test' ? true : false,
 })
